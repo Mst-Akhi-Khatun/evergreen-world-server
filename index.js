88eca6fb897ee8cbdc4bf44a3d9c57b8154b3af3
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2022-03-19 13:58:48 +0600

    done

--- index.js
+++ index.js
@@ -13,22 +13,38 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oxzfl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri);
 
 client.connect(err => {
     const usersCollection = client.db("evergreenWorld").collection("users");
+    const plantsCollection = client.db("evergreenWorld").collection("plants");
+    const ordersCollection = client.db("evergreenWorld").collection("orders");
+
+
+    // All plants
+    app.get("/plants", async (req, res) => {
+        const plants = await plantsCollection.find({}).toArray();
+        res.json(plants);
+    })
+
+    // single plants
+    app.get("/plantDetail/:id", async (req, res) => {
+        const id = req.params.id;
+        const plant = await plantsCollection.findOne({ _id: ObjectId(id) });
+        console.log(plant);
+        res.json(plant);
+    })
 
     // Store user
-    app.post("/user",(req, res) => {
-        const user = usersCollection.insertOne(req.body)
-
+    app.post("/user", async (req, res) => {
+        const user = await usersCollection.insertOne(req.body)
         res.send(user)
-        console.log(user);
     });
 
-
-
-
+    // order a plant
+    app.post("/order", async (req, res) => {
+        const order = await ordersCollection.insertOne(req.body)
+        res.send(order)
+    });
 
     //   client.close();
 });
